7efa1ae94082023b9220e208a988b879642af3c2
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-23 15:46:48 +0200

    Create abstract class

--- Question4.cpp
+++ Question4.cpp
@@ -11,3 +11,31 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+
+#include <iostream> 
+#include <string> 
+#include <cstdio> 
+
+using namespace std;
+
+// abstract class
+class Animal{
+protected: string anName, action;
+public: Animal(){
+	anName = "Animal";
+	action = "Exists";
+}
+		Animal(string nameIn, string actionIn)
+		{
+			anName = nameIn;
+			action = actionIn;
+		}
+
+		~Animal()
+		{
+
+		}
+
+		virtual void name() = 0;
+		virtual void does() = 0;
+};
